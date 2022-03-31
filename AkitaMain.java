a4ab49ba837d9832cc15dd46b49599ce0f2605b4
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-31 12:01:39 +0900

    秋田curry

--- AkitaMain.java
+++ AkitaMain.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class XXXXXMain{
+public class AkitaMain{
 	public static void main(String[] args){
 		String[] curry = {"ポーク","ビーフ","チキン"};
 		final String[] TOPPINGS ={
@@ -24,14 +24,25 @@ public class XXXXXMain{
 			"あげ玉",
 		};
 		//質問をしてカレーのベースを決める
-		
-
+		System.out.println("今夜のカレーを決めます");
+		System.out.print("ベースとなるカレーを選択してください0:ポーク,1:ビーフ,2:チキン>");
+		int base = new Scanner(System.in).nextInt();
 		//トッピングが何種類かを聞く
-
+		System.out.print("トッピングは何種類?>");
+		int topi = new Scanner(System.in).nextInt();
 		//トッピング配列をシャッフル
-
+		for(int i=0;i<TOPPINGS.length-1;i++){
+			int idx=new Random().nextInt(TOPPINGS.length-i)+i;
+			String temp=TOPPINGS[idx];
+			TOPPINGS[idx]=TOPPINGS[i];
+			TOPPINGS[i]=temp;
+		}
 		//トッピング数だけ、配列の頭から取り出しながら文字列連結
-
+		String topiAns = "";
+		for(int i =0;i<topi;i++){
+			topiAns += TOPPINGS[i];
+		}
 		//結果を表示する
+		System.out.printf("今夜のカレーは「%s%sカレー」に決まりました！",topiAns,curry[base]);
 	}
 }
